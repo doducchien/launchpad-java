e4f838d7a3805c6b093349addbd5733ff46b3f54
doducchien <54434667+doducchien@users.noreply.github.com>
2022-06-04 16:28:47 +0700

    fix

--- src/main/java/com/example/launchpadjava/service/MediaService.java
+++
@@ -1,9 +0,0 @@
-package com.example.launchpadjava.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-import com.example.launchpadjava.entity.Media;
-
-public interface MediaService {
-    Media uploadImage(MultipartFile file);
-}
